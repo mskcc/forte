bcd557b2333f3d45c2293b88d9cc7dd068cbc207
Alexandria Pinto <pintoa1@mskcc.org>
2025-08-06 16:29:03 -0400

    version

--- bin/fusion_filtering.R
+++ bin/fusion_filtering.R
@@ -2,7 +2,7 @@
 # __author__      = "Alexandria Dymun"
 # __email__       = "pintoa1@mskcc.org"
 # __contributor__ = "Caryn Hale (halec@mskcc.org)"
-# __version__     = "0.0.1"
+# __version__     = "0.0.2"
 
 
 suppressPackageStartupMessages({
